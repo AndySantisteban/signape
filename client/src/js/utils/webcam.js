5d55ff683cbb815e66e67acf120afd923fe23a1b
AndySantisteban <andyjosue160720@gmail.com>
2023-06-24 12:57:24 -0500

    Feat: remove cluster

--- client/src/js/utils/webcam.js
+++ client/src/js/utils/webcam.js
@@ -1,14 +1,6 @@
 /* The `Webcam` class contains methods to open and close the webcam stream using the `getUserMedia`
 method and the `srcObject` property of a video element. */
 export class Webcam {
-  /* The `open` method is a function that takes a `videoRef` parameter and opens the webcam stream. It
-  first checks if the `navigator.mediaDevices` and `navigator.mediaDevices.getUserMedia` properties
-  are available, which indicate that the browser supports accessing the webcam. If they are
-  available, it calls the `getUserMedia` method with an options object that specifies to use the
-  rear-facing camera (`facingMode: "environment"`) and disable audio (`audio: false`). If the
-  `getUserMedia` method is successful, it sets the `srcObject` property of the `videoRef` to the
-  `stream` object returned by `getUserMedia`, which displays the webcam stream in the `videoRef`. If
-  the `getUserMedia` method fails, it alerts the user that the webcam cannot be opened. */
   open = (videoRef) => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices
--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const config = require("../config");
 const socket = require("./lib/socket");
-const cluster = require("cluster");
-const numCPUs = require("os").cpus().length;
 const compression = require("compression");
 
 const app = express();
@@ -16,21 +14,8 @@ app.use(compression());
 app.use("/", express.static(`${__dirname}/../client/dist`));
 // app.use('/model', express.static(`${__dirname}/../../client/public/best3_web_model/model.json`));
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-
-  // Fork workers
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-  });
-} else {
-  server.listen(config.PORT, () => {
-    socket(server);
-    console.log("Server is listening at :", config.PORT);
-    console.log(`Server in localhost http://localhost:${config.PORT}`);
-  });
-}
+server.listen(config.PORT, () => {
+  socket(server);
+  console.log("Server is listening at :", config.PORT);
+  console.log(`Server in localhost http://localhost:${config.PORT}`);
+});
